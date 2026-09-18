9e5e34b1107836239a2b2fbd7d1a19f6f608cb45
Snehlata kadam <sneha14804@gmail.com>
2026-09-18 11:41:37 +0530

    Update Program.java

--- Program.java
+++ Program.java
@@ -3,6 +3,9 @@ public class Program{
     public void Add(){
         System.out.println("Welcome")
     }
+       public void Sub(){
+        System.out.println("Welcome to Sub")
+    }
 
     public static void main(String[]args){
 
